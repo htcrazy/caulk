c3673cce6468c4043069c6a9a2480becee091c8a
htcrazy <truonghenry1@gmail.com>
2024-03-03 00:16:18 -0800

    updated with new commands

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const select = document.querySelector('.terminal-commands');
-const para = document.querySelector('.box-info');
+const para = document.querySelector('.box-infoz');
 
 select.addEventListener("change", setWeather);
 
@@ -35,13 +35,31 @@ function setWeather() {
             para.textContent = "git commit -m 'message'";
             break;
         case "push":
-            para.textContent = '"git push" or "git push origin main"';
+            para.textContent = '"git push origin *branch-name*" or "git push origin main"';
             break;
         case "git-log":
             para.textContent = "git log";
             break;
         case "git-status":
             para.textContent = "git status";
+            break;
+        case "git-new-branch":
+            para.textContent = "git checkout -b *branch-name*";
+            break;
+        case "branch-check":
+            para.textContent = "git branch";
+            break;
+        case "change-branch":
+            para.textContent = "git checkout *branch-name*";
+            break;
+        case "merge-branch":
+            para.textContent = "git merge *branch-name*";
+            break;
+        case "delete-branch":
+            para.textContent = "git branch -d *branch-name*";
+            break;
+        case "delete-branch-repo":
+            para.textContent = "git push origin --delete *branch-name*";
             break;
         default:
             para.textContent = "";
